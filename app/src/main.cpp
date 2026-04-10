f2be3cc7b2c0b38eb3f7c1850fc89710cd0cc1da
aib <dupa@dupadupa>
2026-04-10 01:39:50 +0000

    Rework code to work on ESP32-C6-DevKitC

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,13 +1,14 @@
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led_strip.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #define SLEEP_TIME_MS 1000
+#define STRIP_NODE DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS 1
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
+static struct led_rgb on = {.r = 0x00, .g = 0x00, .b = 0xFF};
+static struct led_rgb off = {.r = 0x00, .g = 0x00, .b = 0x00};
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -15,12 +16,11 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (!device_is_ready(strip)) return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+
+        led_strip_update_rgb(strip, led_state ? &on : &off, STRIP_NUM_PIXELS);
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
