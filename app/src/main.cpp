ae9d4cc29f9440f6a42119733a30ecdfe9f5fa17
aib <dupa@dupadupa>
2026-06-02 22:44:12 +0000

    Modified dts, added CONFIG_APP_HEARTBEAT_PERIOD_MS and small tweaks

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,11 +3,13 @@
 #include <zephyr/logging/log.h>
 
 #define SLEEP_TIME_MS 1000
-#define STRIP_NODE DT_ALIAS(led_strip)
+#define LED_NODE DT_ALIAS(app_led)
 #define STRIP_NUM_PIXELS 1
 
-static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
-static struct led_rgb on = {.r = 0x00, .g = 0x00, .b = 0xFF};
+static const struct device *const led = DEVICE_DT_GET(LED_NODE);
+
+//structs for rgb led strip: state on with purple color, state off with turned off subpixels
+static struct led_rgb on = {.r = 0xFF, .g = 0x00, .b = 0xFF};
 static struct led_rgb off = {.r = 0x00, .g = 0x00, .b = 0x00};
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
@@ -16,15 +18,15 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!device_is_ready(strip)) return 0;
+    if (!device_is_ready(led)) return 0;
 
     while (1) {
-
-        led_strip_update_rgb(strip, led_state ? &on : &off, STRIP_NUM_PIXELS);
+        //updates led strip with given state based on variable led_state
+        led_strip_update_rgb(led, led_state ? &on : &off, STRIP_NUM_PIXELS);
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
